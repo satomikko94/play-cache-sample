0b2fc1e0967f955641cf1728ae616c834e181fbf
satomikko94 <satomikko94@gmail.com>
2015-12-13 12:57:56 +0900

    Make sample to use cache.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,27 @@
 package controllers;
 
+import model.Task;
 import play.*;
+import play.cache.Cache;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
-    public Result index() {
-        return ok(index.render("Your new application is ready."));
+    static Form<Task> taskFrom = Form.form(Task.class);
+
+    public Result tasks() {
+    	return ok(setCache.render(taskFrom));
+    }
+    
+    public Result setCacheData() {
+        Form<Task> filledForm = taskFrom.bindFromRequest();
+        Task task = filledForm.get();
+
+        Cache.set("task", task, 60 * 1);
+        return ok(setCache.render(filledForm));
     }
 
 }
---
+++ app/model/Task.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Task {
+    public String id;
+    public String taskName;
+}
